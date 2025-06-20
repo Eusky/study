8ce5998c7f4b84193e60a696a487414e493e1d71
eusky <yoochun8128@gmail.com>
2025-06-20 17:54:55 +0900

    20250620 1754

---
+++ Database/01_AboutSQL.sql
@@ -0,0 +1,39 @@
+/*
+SQL 종류
+1. DDL(Data Definition Language)
+  1)데이터 정의어
+  2) 데이터베이스, 테이블, 사용자 등의 정보를 다루는 언어
+  3) 종류
+    (1) CREATE: 생성
+    (2) ALTER: 수정
+    (3) DROP: 삭제
+    (4) TRUNCATE: 테이블의 모든 데이터를 삭제(테이블 구조는 유지, 롤백 불가)
+        
+2. DML(Data Manipulation Language)
+  1) 데이터 조작어
+  2) 실제 데이터를 다루는 언어
+  3) 종류
+    (1) INSERT: 삽입
+    (2) UPDATE: 갱신
+    (3) DELETE: 삭제 
+    
+3. DQL(Data Query Language)
+  1) 데이터 질의어
+  2) 데이터를 검색할 때 사용하는 언어
+  3) 종류
+    (1) SELECT: 선택
+
+4. DCL(Data Control Language)
+  1) 데이터 제어어
+  2) 사용자의 권한을 제어할 때 사용하는 언어
+  3) 종류
+    (1) GRANT: 승인
+    (2) REVOKE: 승인 해제
+    
+5. TCL(Transaction Control Language)
+  1) 트랜잭션 제어어
+  2) 트랜잭션을 처리할 때 사용하는 언어
+  3) 종류
+    (1) COMMIT: 트랜잭션 성공(작업 내용 저장)
+    (2) ROLLBACK: 트랜잭션 실패(작업 내용 취소)
+*/
--- Web/workbook/script.js
+++ Web/workbook/script.js
@@ -27,25 +27,27 @@ const Data = ({index, q}) => {
 
 const Form = ({kind, a, q, as}) => {
 
-  const [userInput, setUserInput] = React.useState("");
-  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const [userInputs, setUserInputs] = React.useState({}); 
+
+  const handleBlur = (e) => {
+    setUserInputs(prev => ({...prev, [e.target.id] : e.target.value}));
+  }
 
   const confirmAnswer = (e) => {
-   
+    const userInput = userInputs[e.target.id.replace("btn-submit", "word")];
+    console.log(userInput, as["no" + e.target.id.at(-1)]);
   }
   const showAnswer = (e) => {
 
   }
-  const saveInput = (e) => {
-   
-  }
+  
 
   const form = Array.from({length: a}, (_, i) => {
   return (
     <React.Fragment key={i}>
       <div>
         <span className="wordNum">{i+1}</span>
-        <input type="text" className="word" id={"word" + kind + (i+1)} onChange={saveInput} value={userInput}/>
+        <input type="text" className="word" id={"word" + kind + (i+1)} onBlur={handleBlur} value={userInputs[i]}/>
         <button type="button" className="btn-submit" id={"btn-submit" + kind + (i+1)} onClick={confirmAnswer}>입력</button>
         <button type="button" className="btn-submit" id={"btn-showAnswers" + kind + (i+1)} onClick={showAnswer}>정답보기</button>
         <span className="isCorrect" id={"isCorrect" + kind + (i+1)}></span>
@@ -59,22 +61,6 @@ const Form = ({kind, a, q, as}) => {
   });
 
   return (<>{form}</>);
-}
-
-const Title = ({text}) => {
-  return (
-    <h1 className="title">{text}</h1>
-  );
-}
-const Nav = () => {
-  return (
-    <div id="nav">
-      <a href="#html">HTML</a>
-      <a href="#css">CSS</a>
-      <a href="#js">JavaScript</a>
-      <a href="#react">React</a>
-    </div>
-  );
 }
 
 const MainPage = () => {
@@ -92,6 +78,21 @@ const MainPage = () => {
       <h2 id="react">React</h2>
       <Form kind={kind[3]} a={answersReactLength} q={questionsReact}/>
     </>
+  );
+}
+const Title = ({text}) => {
+  return (
+    <h1 className="title">{text}</h1>
+  );
+}
+const Nav = () => {
+  return (
+    <div id="nav">
+      <a href="#html">HTML</a>
+      <a href="#css">CSS</a>
+      <a href="#js">JavaScript</a>
+      <a href="#react">React</a>
+    </div>
   );
 }
 ReactDOM.createRoot(document.getElementById("rootTitle")).render(<Title text={"웹(HTML, CSS, JavaScript + React) 단어집"}/>);
@@ -125,140 +126,140 @@ ReactDOM.createRoot(document.getElementById("rootMainPage")).render(<MainPage/>)
 
 
 
-for(let n=0; n<answersCSSLength; n++) {
-  const key = "no" + (n+1);
-  let str = "";
-  questionsCSS[key].forEach(item => str += "<br>&bull; " + item);
+// for(let n=0; n<answersCSSLength; n++) {
+//   const key = "no" + (n+1);
+//   let str = "";
+//   questionsCSS[key].forEach(item => str += "<br>&bull; " + item);
 
-  str = `
-    <div>
-      <span class="wordNum">${n+1}</span>
-      <input type="text" class="word" id="wordCSS${n+1}">
-      <button type="button" class="btn-submit" id="btn-submitCSS${n+1}">입력</button>
-      <button type="button" class="btn-submit" id="btn-showAnswersCSS${n+1}">정답보기</button>
-      <span class="isCorrect" id="isCorrectCSS${n+1}"></span>
-    </div>
-    <div>
-      <span>`
-        + str +
-      `</span>
-    </div>
-    <br>
-  `;
-  containerCSS.innerHTML += str;
-}
+//   str = `
+//     <div>
+//       <span class="wordNum">${n+1}</span>
+//       <input type="text" class="word" id="wordCSS${n+1}">
+//       <button type="button" class="btn-submit" id="btn-submitCSS${n+1}">입력</button>
+//       <button type="button" class="btn-submit" id="btn-showAnswersCSS${n+1}">정답보기</button>
+//       <span class="isCorrect" id="isCorrectCSS${n+1}"></span>
+//     </div>
+//     <div>
+//       <span>`
+//         + str +
+//       `</span>
+//     </div>
+//     <br>
+//   `;
+//   containerCSS.innerHTML += str;
+// }
 
-for(let n=0; n<answersCSSLength; n++) {
-  document.getElementById(`btn-submitCSS${n+1}`).addEventListener("click", () => {
-    const word = document.getElementById(`wordCSS${n+1}`).value;
-    const editedWord = word.replace(/\s+/g, "").toLowerCase();
-    const editedAnswer = answersCSS[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
-    const isCorrect = document.getElementById(`isCorrectCSS${n+1}`);
+// for(let n=0; n<answersCSSLength; n++) {
+//   document.getElementById(`btn-submitCSS${n+1}`).addEventListener("click", () => {
+//     const word = document.getElementById(`wordCSS${n+1}`).value;
+//     const editedWord = word.replace(/\s+/g, "").toLowerCase();
+//     const editedAnswer = answersCSS[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
+//     const isCorrect = document.getElementById(`isCorrectCSS${n+1}`);
     
-    if(editedAnswer.includes(editedWord)) {
-      isCorrect.textContent = "정답";
-      isCorrect.style.color = "#3edc81";
-    }
-    else {
-      isCorrect.textContent = "오답";
-      isCorrect.style.color = "#ff4d4d";
-    }
-  });
-  document.getElementById(`btn-showAnswersCSS${n+1}`).addEventListener("click", () => {
-    const isCorrect = document.getElementById(`isCorrectCSS${n+1}`);
-    isCorrect.textContent = answersCSS[`no${n+1}`][0];
-    isCorrect.style.color = "#3edc81";
-  });
-}
+//     if(editedAnswer.includes(editedWord)) {
+//       isCorrect.textContent = "정답";
+//       isCorrect.style.color = "#3edc81";
+//     }
+//     else {
+//       isCorrect.textContent = "오답";
+//       isCorrect.style.color = "#ff4d4d";
+//     }
+//   });
+//   document.getElementById(`btn-showAnswersCSS${n+1}`).addEventListener("click", () => {
+//     const isCorrect = document.getElementById(`isCorrectCSS${n+1}`);
+//     isCorrect.textContent = answersCSS[`no${n+1}`][0];
+//     isCorrect.style.color = "#3edc81";
+//   });
+// }
 
-for(let n=0; n<answersJSLength; n++) {
-  const key = "no" + (n+1);
-  let str = "";
-  questionsJS[key].forEach(item => str += "<br>&bull; " + item);
+// for(let n=0; n<answersJSLength; n++) {
+//   const key = "no" + (n+1);
+//   let str = "";
+//   questionsJS[key].forEach(item => str += "<br>&bull; " + item);
 
-  str = `
-    <div>
-      <span class="wordNum">${n+1}</span>
-      <input type="text" class="word" id="wordJS${n+1}">
-      <button type="button" class="btn-submit" id="btn-submitJS${n+1}">입력</button>
-      <button type="button" class="btn-submit" id="btn-showAnswersJS${n+1}">정답보기</button>
-      <span class="isCorrect" id="isCorrectJS${n+1}"></span>
-    </div>
-    <div>
-      <span>`
-        + str +
-      `</span>
-    </div>
-    <br>
-  `;
-  containerJS.innerHTML += str;
-}
+//   str = `
+//     <div>
+//       <span class="wordNum">${n+1}</span>
+//       <input type="text" class="word" id="wordJS${n+1}">
+//       <button type="button" class="btn-submit" id="btn-submitJS${n+1}">입력</button>
+//       <button type="button" class="btn-submit" id="btn-showAnswersJS${n+1}">정답보기</button>
+//       <span class="isCorrect" id="isCorrectJS${n+1}"></span>
+//     </div>
+//     <div>
+//       <span>`
+//         + str +
+//       `</span>
+//     </div>
+//     <br>
+//   `;
+//   containerJS.innerHTML += str;
+// }
 
-for(let n=0; n<answersJSLength; n++) {
-  document.getElementById(`btn-submitJS${n+1}`).addEventListener("click", () => {
-    const word = document.getElementById(`wordJS${n+1}`).value;
-    const editedWord = word.replace(/\s+/g, "").toLowerCase();
-    const editedAnswer = answersJS[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
-    const isCorrect = document.getElementById(`isCorrectJS${n+1}`);
+// for(let n=0; n<answersJSLength; n++) {
+//   document.getElementById(`btn-submitJS${n+1}`).addEventListener("click", () => {
+//     const word = document.getElementById(`wordJS${n+1}`).value;
+//     const editedWord = word.replace(/\s+/g, "").toLowerCase();
+//     const editedAnswer = answersJS[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
+//     const isCorrect = document.getElementById(`isCorrectJS${n+1}`);
     
-    if(editedAnswer.includes(editedWord)) {
-      isCorrect.textContent = "정답";
-      isCorrect.style.color = "#3edc81";
-    }
-    else {
-      isCorrect.textContent = "오답";
-      isCorrect.style.color = "#ff4d4d";
-    }
-  });
-  document.getElementById(`btn-showAnswersJS${n+1}`).addEventListener("click", () => {
-    const isCorrect = document.getElementById(`isCorrectJS${n+1}`);
-    isCorrect.textContent = answersJS[`no${n+1}`][0];
-    isCorrect.style.color = "#3edc81";
-  });
-}
+//     if(editedAnswer.includes(editedWord)) {
+//       isCorrect.textContent = "정답";
+//       isCorrect.style.color = "#3edc81";
+//     }
+//     else {
+//       isCorrect.textContent = "오답";
+//       isCorrect.style.color = "#ff4d4d";
+//     }
+//   });
+//   document.getElementById(`btn-showAnswersJS${n+1}`).addEventListener("click", () => {
+//     const isCorrect = document.getElementById(`isCorrectJS${n+1}`);
+//     isCorrect.textContent = answersJS[`no${n+1}`][0];
+//     isCorrect.style.color = "#3edc81";
+//   });
+// }
 
-for(let n=0; n<answersReactLength; n++) {
-  const key = "no" + (n+1);
-  let str = "";
-  questionsReact[key].forEach(item => str += "<br>&bull; " + item);
+// for(let n=0; n<answersReactLength; n++) {
+//   const key = "no" + (n+1);
+//   let str = "";
+//   questionsReact[key].forEach(item => str += "<br>&bull; " + item);
 
-  str = `
-    <div>
-      <span class="wordNum">${n+1}</span>
-      <input type="text" class="word" id="wordReact${n+1}">
-      <button type="button" class="btn-submit" id="btn-submitReact${n+1}">입력</button>
-      <button type="button" class="btn-submit" id="btn-showAnswersReact${n+1}">정답보기</button>
-      <span class="isCorrect" id="isCorrectReact${n+1}"></span>
-    </div>
-    <div>
-      <span>`
-        + str +
-      `</span>
-    </div>
-    <br>
-  `;
-  containerReact.innerHTML += str;
-}
+//   str = `
+//     <div>
+//       <span class="wordNum">${n+1}</span>
+//       <input type="text" class="word" id="wordReact${n+1}">
+//       <button type="button" class="btn-submit" id="btn-submitReact${n+1}">입력</button>
+//       <button type="button" class="btn-submit" id="btn-showAnswersReact${n+1}">정답보기</button>
+//       <span class="isCorrect" id="isCorrectReact${n+1}"></span>
+//     </div>
+//     <div>
+//       <span>`
+//         + str +
+//       `</span>
+//     </div>
+//     <br>
+//   `;
+//   containerReact.innerHTML += str;
+// }
 
-for(let n=0; n<answersReactLength; n++) {
-  document.getElementById(`btn-submitReact${n+1}`).addEventListener("click", () => {
-    const word = document.getElementById(`wordReact${n+1}`).value;
-    const editedWord = word.replace(/\s+/g, "").toLowerCase();
-    const editedAnswer = answersReact[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
-    const isCorrect = document.getElementById(`isCorrectReact${n+1}`);
+// for(let n=0; n<answersReactLength; n++) {
+//   document.getElementById(`btn-submitReact${n+1}`).addEventListener("click", () => {
+//     const word = document.getElementById(`wordReact${n+1}`).value;
+//     const editedWord = word.replace(/\s+/g, "").toLowerCase();
+//     const editedAnswer = answersReact[`no${n+1}`].map(str => str.replace(/\s+/g, "").toLowerCase());
+//     const isCorrect = document.getElementById(`isCorrectReact${n+1}`);
     
-    if(editedAnswer.includes(editedWord)) {
-      isCorrect.textContent = "정답";
-      isCorrect.style.color = "#3edc81";
-    }
-    else {
-      isCorrect.textContent = "오답";
-      isCorrect.style.color = "#ff4d4d";
-    }
-  });
-  document.getElementById(`btn-showAnswersReact${n+1}`).addEventListener("click", () => {
-    const isCorrect = document.getElementById(`isCorrectReact${n+1}`);
-    isCorrect.textContent = answersReact[`no${n+1}`][0];
-    isCorrect.style.color = "#3edc81";
-  });
-}
+//     if(editedAnswer.includes(editedWord)) {
+//       isCorrect.textContent = "정답";
+//       isCorrect.style.color = "#3edc81";
+//     }
+//     else {
+//       isCorrect.textContent = "오답";
+//       isCorrect.style.color = "#ff4d4d";
+//     }
+//   });
+//   document.getElementById(`btn-showAnswersReact${n+1}`).addEventListener("click", () => {
+//     const isCorrect = document.getElementById(`isCorrectReact${n+1}`);
+//     isCorrect.textContent = answersReact[`no${n+1}`][0];
+//     isCorrect.style.color = "#3edc81";
+//   });
+// }
